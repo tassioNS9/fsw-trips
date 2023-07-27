d7e223ec11f9d3d16d4f51bb94369f6d9ebee4c2
tassions@hotmail.com <tassions@hotmail.com>
2023-07-27 08:35:00 -0300

    add links and icons

--- src/app/components/RecommededTrips.tsx
+++ src/app/components/RecommededTrips.tsx
@@ -4,28 +4,28 @@ import { Trip } from "@prisma/client";
 import React from "react";
 
 async function getTrips() {
- const trips = await prisma.trip.findMany({});
+  const trips = await prisma.trip.findMany({});
 
-return trips;
+  return trips;
 }
 
 const RecommendedTrips = async () => {
   const data = await getTrips();
+  console.log(data)
 
   return (
-    <div className="container mx-auto p-5">
+    <div className="container mx-auto p-5" >
       <div className="flex items-center">
         <div className="w-full h-[1px] bg-grayLighter"></div>
         <h2 className="px-5 font-medium text-grayPrimary whitespace-nowrap">Destinos Recomendados</h2>
         <div className="w-full h-[1px] bg-grayLighter"></div>
       </div>
 
-      <div className="flex flex-col items-center mt-5 gap-5">
-        {data.map((trip :Trip) =>(
-          <TripItem key={trip.id} trip={trip}/> 
-        ))}   
+      <div className="flex flex-col items-center mt-5 lg:mt-12 gap-5 lg:flex-row gap lg:flex-wrap lg:justify-center  lg:gap-10">
+        {data.map((trip: Trip) => (
+          <TripItem key={trip.id} trip={trip} />
+        ))}
       </div>
-
     </div>
   );
 };
