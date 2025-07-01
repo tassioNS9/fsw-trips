4b300d0e46a1eb6be00967ebe6d0a867c6be11b2
Tassio Neves <tassions@hotmail.com>
2025-06-30 21:22:56 -0300

    feat: add dynamic in auth-next

--- src/app/my-trips/page.tsx
+++ src/app/my-trips/page.tsx
@@ -21,7 +21,9 @@ const MyTrips = () => {
   const router = useRouter();
 
   const fetchReservations = async () => {
-    const response = await fetch(`/api/user/${(data?.user as any)?.id}/reservations`);
+    const response = await fetch(
+      `/api/user/${(data?.user as any)?.id}/reservations`
+    );
 
     const json = await response.json();
 
@@ -38,16 +40,24 @@ const MyTrips = () => {
 
   return (
     <div className="container mx-auto p-5">
-      <h1 className="font-semibold text-primaryDarker text-xl lg:mb-5">Minhas Viagens</h1>
+      <h1 className="font-semibold text-primaryDarker text-xl lg:mb-5">
+        Minhas Viagens
+      </h1>
       {reservations.length > 0 ? (
         <div className="flex flex-col lg:grid lg:grid-cols-3 lg:gap-14">
           {reservations?.map((reservation) => (
-            <UserReservationItem fetchReservations={fetchReservations} key={reservation.id} reservation={reservation} />
+            <UserReservationItem
+              fetchReservations={fetchReservations}
+              key={reservation.id}
+              reservation={reservation}
+            />
           ))}
         </div>
       ) : (
         <div className="flex flex-col lg:max-w-[500px]">
-          <p className="mt-2 font-medium text-primaryDarker">Você ainda não tem nenhuma reserva! =(</p>
+          <p className="mt-2 font-medium text-primaryDarker">
+            Você ainda não tem nenhuma reserva! =(
+          </p>
 
           <Link href="/">
             <Button className="w-full mt-2 lg:mt-5">Fazer reserva</Button>
