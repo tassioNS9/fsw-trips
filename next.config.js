8572cbdd8da3bd8fa95d26b9d7da5a295b97724d
Tassio Neves <tassions@hotmail.com>
2025-06-30 21:01:41 -0300

    fix: config next-config

--- next.config.js
+++ next.config.js
@@ -1,22 +1,24 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    experimental: {
-        appDir: true,
+  experimental: {
+    appDir: true,
+    serverActions: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "lh3.googleusercontent.com",
+        port: "",
       },
-      images: {
-        remotePatterns: [
-          {
-            protocol: "https",
-            hostname: "lh3.googleusercontent.com",
-            port: "",
-          },
-          {
-            protocol: "https",
-            hostname: "images.unsplash.com",
-            port: "",
-          },
-        ],
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+        port: "",
       },
-}
+    ],
+  },
+  output: "standalone", // para compatibilidade com Vercel
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
