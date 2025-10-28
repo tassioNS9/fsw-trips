e3069a50d173670130fab6943750ddabaff9824c
Tassio Neves <tassions@hotmail.com>
2025-10-27 21:20:55 -0300

    style: format DatePicker component for improved readability

--- src/components/DatePicker.tsx
+++ src/components/DatePicker.tsx
@@ -1,6 +1,9 @@
 import { LegacyRef, forwardRef } from "react";
 import { twMerge } from "tailwind-merge";
-import _DatePicker, { ReactDatePickerProps, registerLocale } from "react-datepicker";
+import _DatePicker, {
+  ReactDatePickerProps,
+  registerLocale,
+} from "react-datepicker";
 import ptBR from "date-fns/locale/pt-BR";
 
 import "react-datepicker/dist/react-datepicker.css";
@@ -12,7 +15,10 @@ interface InputProps extends ReactDatePickerProps {
   errorMessage?: string;
 }
 
-function DatePicker({ className, error, errorMessage, ...props }: InputProps, ref: LegacyRef<HTMLInputElement> | undefined) {
+function DatePicker(
+  { className, error, errorMessage, ...props }: InputProps,
+  ref: LegacyRef<HTMLInputElement> | undefined
+) {
   const datePickerClassName = twMerge(
     "rounded-lg border border-gray-300 bg-white p-2 text-sm font-normal text-primaryDarker placeholder-black placeholder-opacity-20 outline-none transition-all focus:ring-1 focus:ring-primary",
     error ? "border-red-500" : "",
@@ -21,8 +27,17 @@ function DatePicker({ className, error, errorMessage, ...props }: InputProps, re
 
   return (
     <div className="flex w-full flex-col">
-      <_DatePicker dateFormat="dd/MM/yyyy" locale="pt-BR" wrapperClassName="w-full" className={datePickerClassName} enableTabLoop={false} {...props} />
-      {error && errorMessage && <div className="text-red-500 mt-1 text-xs">{errorMessage}</div>}
+      <_DatePicker
+        dateFormat="dd/MM/yyyy"
+        locale="pt-BR"
+        wrapperClassName="w-full"
+        className={datePickerClassName}
+        enableTabLoop={false}
+        {...props}
+      />
+      {error && errorMessage && (
+        <div className="text-red-500 mt-1 text-xs">{errorMessage}</div>
+      )}
     </div>
   );
 }
