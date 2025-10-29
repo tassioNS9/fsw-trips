63934baf9b01ea52fb7f92f59ba6a140007ab778
Tassio Neves <tassions@hotmail.com>
2025-10-28 21:06:38 -0300

    refactor: simplify UserReservationItem component by removing unused fetchReservations prop and improving formatting

--- src/app/my-trips/components/UserReservationItem.tsx
+++ src/app/my-trips/components/UserReservationItem.tsx
@@ -6,7 +6,6 @@ import { format } from "date-fns";
 import { ptBR } from "date-fns/locale";
 import Button from "@/components/Button";
 import { toast } from "react-toastify";
-import { useRouter } from "next/navigation";
 
 interface UserReservationItemProps {
   reservation: Prisma.TripReservationGetPayload<{
@@ -15,9 +14,7 @@ interface UserReservationItemProps {
   fetchReservations: () => void;
 }
 
-const UserReservationItem = ({ reservation, fetchReservations }: UserReservationItemProps) => {
-  const router = useRouter();
-
+const UserReservationItem = ({ reservation }: UserReservationItemProps) => {
   const { trip } = reservation;
 
   const handleDeleteClick = async () => {
@@ -29,9 +26,11 @@ const UserReservationItem = ({ reservation, fetchReservations }: UserReservation
       return toast.error("Ocorreu um erro ao cancelar a reserva!");
     }
 
-    toast.success("Reserva cancelada com sucesso!", { position: "bottom-center" });
+    toast.success("Reserva cancelada com sucesso!", {
+      position: "bottom-center",
+    });
 
-    fetchReservations();
+    window.location.reload();
   };
 
   return (
@@ -40,14 +39,24 @@ const UserReservationItem = ({ reservation, fetchReservations }: UserReservation
       <div className="flex flex-col p-5 mt-5 border-grayLighter border-solid border shadow-lg rounded-lg">
         <div className="flex items-center gap-3 pb-5 border-b border-grayLighter border-solid">
           <div className="relative h-[106px] w-[124px]">
-            <Image src={trip.coverImage} fill style={{ objectFit: "cover" }} className="rounded-lg" alt={trip.name} />
+            <Image
+              src={trip.coverImage}
+              fill
+              style={{ objectFit: "cover" }}
+              className="rounded-lg"
+              alt={trip.name}
+            />
           </div>
 
           <div className="flex flex-col">
-            <h2 className="text-xl text-primaryDarker font-semibold">{trip.name}</h2>
+            <h2 className="text-xl text-primaryDarker font-semibold">
+              {trip.name}
+            </h2>
             <div className="flex items-center gap-1">
               <ReactCountryFlag countryCode={trip.countryCode} svg />
-              <p className="text-xs text-grayPrimary underline">{trip.location}</p>
+              <p className="text-xs text-grayPrimary underline">
+                {trip.location}
+              </p>
             </div>
           </div>
         </div>
@@ -55,19 +64,31 @@ const UserReservationItem = ({ reservation, fetchReservations }: UserReservation
         <div className="flex flex-col mt-5 text-primaryDarker">
           <h3 className="text-sm">Data</h3>
           <div className="flex items-center gap-1">
-            <p className="text-sm">{format(new Date(reservation.startDate), "dd 'de' MMMM", { locale: ptBR })}</p>
+            <p className="text-sm">
+              {format(new Date(reservation.startDate), "dd 'de' MMMM", {
+                locale: ptBR,
+              })}
+            </p>
             {" - "}
-            <p className="text-sm">{format(new Date(reservation.endDate), "dd 'de' MMMM", { locale: ptBR })}</p>
+            <p className="text-sm">
+              {format(new Date(reservation.endDate), "dd 'de' MMMM", {
+                locale: ptBR,
+              })}
+            </p>
           </div>
 
           <h3 className="mt-5 text-sm">Hóspedes</h3>
           <p className="text-sm pb-5">{reservation.guests} hóspedes</p>
 
-          <h3 className="font-semibold text-primaryDarker mt-3 pt-5 border-t border-solid border-grayLighter">Informações sobre o preço</h3>
+          <h3 className="font-semibold text-primaryDarker mt-3 pt-5 border-t border-solid border-grayLighter">
+            Informações sobre o preço
+          </h3>
 
           <div className="flex justify-between mt-1">
             <p className="text-primaryDarker text-sm mt-2">Total:</p>
-            <p className="font-medium text-sm">R${Number(reservation.totalPaid)}</p>
+            <p className="font-medium text-sm">
+              R${Number(reservation.totalPaid)}
+            </p>
           </div>
 
           <Button variant="danger" className="mt-5" onClick={handleDeleteClick}>
